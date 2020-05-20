e2ef4a85ea3c5e52b644522a1798af7f2b33de1e
artis04 <49913718+artis04@users.noreply.github.com>
2020-05-21 00:04:29 +0300

    V1.6

--- index.js
+++ index.js
@@ -10,19 +10,7 @@ const client = new Discord.Client();
  client.runtime
  */
 
-// Database.createTables(sqlite3, client);  // creates database with required tables if not created yet.
-
 client.login(config.token);
-//console.log(client.channels.cache.for);
-// client.channels.cache.forEach(channel => {
-//     if(channel.type === 'text'){
-//         textChannels.push(channel.name);
-//     }
-//     console.log(textChannels);
-// });
-// console.log(client.channels.cache);
-
-
 
 client.on('ready', () => {
     let textChannels = []
@@ -30,13 +18,13 @@ client.on('ready', () => {
         if(channel.type === 'text'){
             textChannels.push(channel.name);
         }
-    })
-    Database.createTables(sqlite3, textChannels);
+    });
+    Database.createTables(sqlite3, textChannels); // creates database with required tables if not created yet.
 });
 
 
-function getAllMentionedUsersOrChannels(message, persons){
-    if(persons){
+function getAllMentionedUsersOrChannels(message, getUsers){
+    if(getUsers){
         split_string = "<@!";
     }else{
         split_string = "<#";
@@ -45,7 +33,7 @@ function getAllMentionedUsersOrChannels(message, persons){
     var specificID = message.content.split(split_string);
     let mentioned = [];
     for(var i = 1; i < specificID.length; i++){
-        try{ // in case there is no valid @mention user id (then do catch == skip this.)
+        try{ // in case there is no valid @mention id (then do catch == skip this.)
 
             var valid_id = specificID[i].substring(0, specificID[i].indexOf(">"))
             if (!mentioned.includes(valid_id)){  // pervents to spam one user or channel multiple times at once.
@@ -59,12 +47,6 @@ function getAllMentionedUsersOrChannels(message, persons){
 
 client.on('message', message => {
     var ownerOrSensei = false;
-    
-    // console.log(message.channels);
-    for(i=0;i<message.guild.channels.length;i++){
-        console.log(message.guild.channels[i].type);
-    }
-
     if(message.member.roles.cache.find(role => role.name === "Owner") || message.member.roles.cache.find(role => role.name === "Sensei")) {
         ownerOrSensei = true;  // checks if message was sent from owner or Sensei (Role named Owner or Sensei)
     }
@@ -82,48 +64,42 @@ client.on('message', message => {
             positiveVote = false;
         };
      
-        let textChannels = []
-        message.guild.channels.cache.forEach(channel => {
-            if(channel.type === 'text'){
-                textChannels.push(channel.name);
-            }
-        });
-        
         for(i = 0; i < voted_users.length; i++){
             var userInfo = client.users.fetch(voted_users[i]);
             // userInfo contains id; is_bot?; username; discriminator; avatarID; flags; lastmessageid; lastmessagechannelid
             userInfo.then(user => {
-                Database.addPoints(sqlite3, user.id, user.username, positiveVote, textChannels, user);
+                Database.addPoints(sqlite3, user.id, user.username, positiveVote, user);
             }).catch(console.error);
         };
 
 
     }else if(message.content.toLowerCase().startsWith("!points")){
-        console.log(message.content);
         let voted_users = getAllMentionedUsersOrChannels(message, true);
         let channels = getAllMentionedUsersOrChannels(message, false);
-        console.log(voted_users);
-        console.log(channels);
 
+        if(voted_users.length === 0){
+            voted_users = [message.author.id];
+        }
+        // changes channels ID to channels name
         let mentionedTextChannels = []
         message.guild.channels.cache.forEach(channel => {
             if(channels.includes(channel.id)){
-                mentionedTextChannels.push(channel.name);
+                mentionedTextChannels.push(channel);
             }
-
         });
-        console.log(client.users.fetch(voted_users[0]));
-
 
         for(i=0; i < voted_users.length; i++){
             var userInfo = client.users.fetch(voted_users[i]);
             userInfo.then(async user => {
-                for(i = 0; i < channels.length; i++){
-                    
+                if(mentionedTextChannels.length === 0){
+                    Database.sendUserPoints(sqlite3, user,message, undefined);
+                }else{
+                    for(i = 0; i < mentionedTextChannels.length; i++){
+                        Database.sendUserPoints(sqlite3, user, message, mentionedTextChannels[i]);
+                    }
                 }
-
-                Database.sendUserPoints(sqlite3, user, message, user, textChannels);
             }).catch(console.error);
         }
     }
 });
+ 
